1abb2dba256592c98920041f9cd32e53cb1f2645
nachovidondo <nachovidondo@gmail.com>
2021-04-25 18:02:55 -0300

    Administration model and settings config

--- Building_Administration/Administration/models.py
+++ Building_Administration/Administration/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Building(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50 ,verbose_name="Nombre de Edificio")
+    class Meta:
+        verbose_name ="Edificio"
+        verbose_name_plural = "Edificios"
+        
+    def __str__(self):
+        return self.name
+
+
+ 
+class Apartament(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=200,unique=True, verbose_name="Codigo")
+    ph = models.CharField(max_length=50, verbose_name="PH")
+    address = models.CharField(max_length=200,verbose_name="Numeracion")
+    average = models.FloatField(verbose_name="Porcentaje Correspondiente")
+    balance_due = models.FloatField(verbose_name="Deuda")
+    building = models.ForeignKey(Building, verbose_name="Edificio",
+                                 on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name ="Departamento"
+        verbose_name_plural ="Departamentos"
+    
+    def __str__(self):
+        return self.ph
+    
