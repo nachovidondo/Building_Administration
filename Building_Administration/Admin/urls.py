4dad28f93febaf9c7091f8321c6f61b57696a13d
nachovidondo <nachovidondo@gmail.com>
2021-04-27 19:15:18 -0300

    Apartaments list

--- Building_Administration/Admin/urls.py
+++ Building_Administration/Admin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Index
+from .views import Index, ApartamentsList
 
 
 
 urlpatterns = [
-    path('index', Index.as_view(), name='index')
+    path('index', Index.as_view(), name='index'),
+    path('apartaments_list', ApartamentsList.as_view(), name='apartaments_list')
 ]
--- Building_Administration/Admin/views.py
+++ Building_Administration/Admin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from Administration.models import Building
+from Administration.models import Building, Apartament
 from django.views.generic  import ListView
 
 
@@ -9,3 +9,13 @@ class Index(ListView):
     template_name ='index.html'
     context_object_name = 'buildings'
     
+class ApartamentsList(ListView):
+    model = Apartament
+    template_name = 'apartaments_list.html'
+    context_object_name = 'apartaments'
+    def get_queryset(self):
+        qs = Apartament.objects.all()
+        building_id = self.request.GET.get("building")
+        if building_id:
+            apartament = qs.filter(building__id=building_id)
+        return apartament
